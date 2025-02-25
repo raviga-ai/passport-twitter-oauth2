24d48d052a23771489758247aef991237d8327c7
Pallav Bajjuri <pallav@ipverse.in>
2025-02-25 21:04:48 +0530

    updated tests

--- test/strategy.tests.js
+++ test/strategy.tests.js
@@ -8,14 +8,28 @@ const profileExample = {
     id: '2244994945',
     name: 'John Doe',
     username: 'johndoe',
+    description: 'Test bio',
+    location: 'San Francisco, CA',
+    profile_image_url: 'https://example.com/image.jpg',
+    verified: true,
+    verified_type: 'blue',
+    protected: false,
+    public_metrics: {
+      followers_count: 1000,
+      following_count: 500
+    },
+    url: 'https://example.com'
   }
 };
 
 nock.disableNetConnect();
 
 describe('Twitter OAuth2 Strategy', function () {
+  this.timeout(5000); // Increase timeout to 5 seconds
+  
   const origin = 'https://api.twitter.com';
   const profilePath = '/2/users/me';
+  const defaultFields = 'description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,verified_type,withheld';
 
   it('sanity check', function (done) {
     const options = {
@@ -35,8 +49,8 @@ describe('Twitter OAuth2 Strategy', function () {
   describe('userProfile(accessToken, done)', function () {
     context('with user.read scope', function () {
       beforeEach(function () {
-        this.scope = nock(origin)
-          .get(profilePath)
+        nock(origin)
+          .get(`${profilePath}?user.fields=${defaultFields}`)
           .matchHeader('Authorization', 'Bearer test_access_token')
           .reply(200, profileExample);
       });
@@ -45,7 +59,7 @@ describe('Twitter OAuth2 Strategy', function () {
         nock.cleanAll();
       });
 
-      it('passes id, username and display name to callback', function (done) {
+      it('passes profile data to callback', function (done) {
         const options = {
           clientID: 'test_client_id',
           clientSecret: 'test_client_secret',
@@ -56,22 +70,36 @@ describe('Twitter OAuth2 Strategy', function () {
         const st = new Strategy(options, function () {});
 
         st.userProfile('test_access_token', function (err, profile) {
-          should.not.exist(err);
-          profile.provider.should.eql('twitter');
-          profile.id.should.eql('2244994945');
-          profile.username.should.eql('johndoe');
-          profile.displayName.should.eql('John Doe');
-          profile._raw.should.be.type('string');
-          profile._json.should.be.type('object');
-          done();
+          if (err) return done(err);
+          
+          try {
+            should.not.exist(err);
+            profile.provider.should.eql('twitter');
+            profile.id.should.eql('2244994945');
+            profile.username.should.eql('johndoe');
+            profile.displayName.should.eql('John Doe');
+            profile.description.should.eql('Test bio');
+            profile.location.should.eql('San Francisco, CA');
+            profile.photos[0].value.should.eql('https://example.com/image.jpg');
+            profile.verified.should.eql(true);
+            profile.verifiedType.should.eql('blue');
+            profile.protected.should.eql(false);
+            profile.metrics.should.be.type('object');
+            profile.url.should.eql('https://example.com');
+            profile._raw.should.be.type('string');
+            profile._json.should.be.type('object');
+            done();
+          } catch (e) {
+            done(e);
+          }
         });
       });
     });
 
     context('when error occurs', function () {
       beforeEach(function () {
-        this.scope = nock(origin)
-          .get(profilePath)
+        nock(origin)
+          .get(`${profilePath}?user.fields=${defaultFields}`)
           .matchHeader('Authorization', 'Bearer test_access_token')
           .reply(500, {
             errors: [{ message: 'Internal Server Error' }]
@@ -93,9 +121,13 @@ describe('Twitter OAuth2 Strategy', function () {
         const st = new Strategy(options, function () {});
 
         st.userProfile('test_access_token', function (err, profile) {
-          should.exist(err);
-          should.not.exist(profile);
-          done();
+          try {
+            should.exist(err);
+            should.not.exist(profile);
+            done();
+          } catch (e) {
+            done(e);
+          }
         });
       });
     });
